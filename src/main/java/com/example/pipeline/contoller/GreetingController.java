4c4da0ccad73fe1f8589206f04dd033514770e20
SHTUNDER <shtunder.andrey1yandex.by>
2022-09-12 15:28:57 +0300

    Second commit

--- src/main/java/com/example/pipeline/contoller/GreetingController.java
+++ src/main/java/com/example/pipeline/contoller/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+    // Some changes
     @GetMapping("/hello")
     public String hello(){
         return "Demo application with Spring Boot!";
