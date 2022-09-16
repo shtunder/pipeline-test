522177d908cd372c1811af54c60414b4e56d2b27
SHTUNDER <shtunder.andrey1yandex.by>
2022-09-17 00:51:04 +0300

    First commit v.4

--- src/main/java/com/example/pipeline/contoller/TestController.java
+++ src/main/java/com/example/pipeline/contoller/TestController.java
@@ -4,15 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class TestController {
 
-    // Some changes
-    // New comment
-    // New changes
+    @GetMapping("/")
+    public String health() {
+        return "Hello & Welcome to Bukable !!!";
+    }
+
     @GetMapping("/hello")
     public String hello(){
         return "Demo application with Spring Boot!";
     }
-
 
 }
